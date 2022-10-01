acbf729e777ed59afbfcd3dc9eb1100ee1d79e43
charuzain <charuabhizain@gmail.com>
2022-10-01 00:02:02 -0400

    Display number

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -8,15 +8,38 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property float currentValue;
+@property float value;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *btn;
+
+@property BOOL isNewNumber;
 
 @end
 
 @implementation ViewController
+- (IBAction)btnPressed:(UIButton *)sender {
+    if(self.isNewNumber){
+        self.label.text = sender.titleLabel.text;
+        self.isNewNumber = NO;
+       
+    }
+    else{
+        self.label.text = [ NSString stringWithFormat:@"%@%@" , self.label.text , sender.titleLabel.text];
+        
+        
+    }
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.isNewNumber = YES;
+    
 }
 
+   
 
 @end
