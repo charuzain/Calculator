d4bdf0d65de5e23bb7054004f7061042f7ec6003
charuzain <charuabhizain@gmail.com>
2022-10-01 22:02:02 -0400

    Implement calculator

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -6,41 +6,64 @@
 //
 
 #import "ViewController.h"
+#import "Calculator_Brain.h"
+
+
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *label;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *btn;
 @property BOOL isNewNumber;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *operation;
-@property double result;
-@property double currentValue;
+@property (strong , nonatomic) Calculator_Brain * myModel;
+
+
 
 @end
 
 @implementation ViewController
+
+-(Calculator_Brain *)myModel{
+    if(_myModel == nil){
+        _myModel= [[Calculator_Brain alloc]init];
+    }
+    return _myModel;
+}
+
+
 - (IBAction)btnPressed:(UIButton *)sender {
     if(self.isNewNumber){
         self.label.text = sender.titleLabel.text;
         self.isNewNumber = NO;
-        self.currentValue = [sender.titleLabel.text doubleValue];
-
+      
     }
     else{
         self.label.text = [ NSString stringWithFormat:@"%@%@" , self.label.text , sender.titleLabel.text];
-        self.currentValue = [sender.titleLabel.text doubleValue];
-        
     }
     
+   
 }
+
+
 - (IBAction)clear:(UIButton *)sender {
     self.label.text = [ NSString stringWithFormat:@"%d" , 0];
        self.isNewNumber = YES;
 
 }
+- (IBAction)pressEnter {
     
+    double value = self.label.text.doubleValue ;
+    [self.myModel pushItem:value];
+    self.isNewNumber = YES;
+
+}
+
 - (IBAction)operatorSelected:(UIButton *)sender {
-    
- 
+    NSString * operator = sender.titleLabel.text;
+    NSLog(@"%@" , operator);
+    double result = [self.myModel calculate:operator];
+    NSLog(@"%f" , result);
+    self.label.text = [NSString stringWithFormat:@"%.2f" , result];
     
 }
 
